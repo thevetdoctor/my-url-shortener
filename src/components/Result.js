01c7b5d6cb7a8b7c099cd5e51220d957053285cb
Linda Okorie <lindaokorie27@gmail.com>
2021-07-15 16:11:32 +0100

    added content to Result component

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import '../styles/Result.scss'
 
 const Result = (props) => {
     return (
-        <div>
-            
+        <div className="result">
+          <span className="fullLink">{props.fullLink}</span>
+          <div className="shortenedDetails">
+            <span className="shortenedLink">{props.shortenedLink}</span>
+            <a href="#" className="btn">Copy</a>
+          </div>
         </div>
     )
 }
