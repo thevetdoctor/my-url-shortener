a2e31e91d731e4aec4ef61460f80d24b2e8e69b5
Linda Okorie <lindaokorie27@gmail.com>
2021-08-28 12:54:45 +0100

    conditionally displayed results of shortening

--- src/components/ResultList/ResultList.js
+++ src/components/ResultList/ResultList.js
@@ -6,12 +6,14 @@ import "./ResultsList.scss";
 
 const ResultList = (props) => {
   const shortenedResults = props.results;
-  const listItems = shortenedResults.map((item) => (
-    <Result
-      fullLink={item.original_link}
-      shortenedLink={item.full_short_link}
-    />
-  ));
+  const listItems = shortenedResults
+    ? shortenedResults.map((item) => (
+        <Result
+          fullLink={item.original_link}
+          shortenedLink={item.full_short_link}
+        />
+      ))
+    : "";
   return (
     <div data-testid="results-list" id="results" className="container">
       {listItems}
